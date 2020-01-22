e80f30904d663545c01d604573d1a8c00e842df4
Cristiano Fanelli <cfanelli@mit.edu>
2020-01-22 18:13:49 -0500

    Fixing set energy

--- src/libraries/FT/FTCalHit.h
+++ src/libraries/FT/FTCalHit.h
@@ -9,22 +9,22 @@
 // [ Source ]
 // [ Revision ]
 //
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-// Jefferson Science Associates LLC Copyright Notice:  
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// Jefferson Science Associates LLC Copyright Notice:
 // Copyright 251 2014 Jefferson Science Associates LLC All Rights Reserved. Redistribution
 // and use in source and binary forms, with or without modification, are permitted as a
-// licensed user provided that the following conditions are met:  
+// licensed user provided that the following conditions are met:
 // 1. Redistributions of source code must retain the above copyright notice, this
-//    list of conditions and the following disclaimer. 
+//    list of conditions and the following disclaimer.
 // 2. Redistributions in binary form must reproduce the above copyright notice, this
 //    list of conditions and the following disclaimer in the documentation and/or other
-//    materials provided with the distribution.  
+//    materials provided with the distribution.
 // 3. The name of the author may not be used to endorse or promote products derived
-//    from this software without specific prior written permission.  
+//    from this software without specific prior written permission.
 // This material resulted from work developed under a United States Government Contract.
 // The Government retains a paid-up, nonexclusive, irrevocable worldwide license in such
 // copyrighted data to reproduce, distribute copies to the public, prepare derivative works,
-// perform publicly and display publicly and to permit others to do so.   
+// perform publicly and display publicly and to permit others to do so.
 // THIS SOFTWARE IS PROVIDED BY JEFFERSON SCIENCE ASSOCIATES LLC "AS IS" AND ANY EXPRESS
 // OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 // MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
@@ -35,7 +35,7 @@
 // LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
 // OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 #ifndef _FTCalHit_h_
 #define _FTCalHit_h_
@@ -43,6 +43,8 @@
 #include "TT/TranslationTable.h"
 #include "DAQ/chronoTypeDef.h"
 #include <JANA/JObject.h>
+#include <JANA/JFactory.h>
+
 //////////////////////////////////////////////////////////////////////////////////////////////////
 /// Brief class description.
 ///
@@ -53,22 +55,29 @@ public:
 	FTCalHit();
 	virtual ~FTCalHit();
 
-	float getEnergy() const {
+	float getHitEnergy() const {
 		return energy;
 	}
 
-	void setEnergy(float charge) {
-		this->energy = energy;
+	void setHitEnergy(float charge) {
+		this->energy = charge;
 	}
 
-	T4nsec getTime() const {
+	T4nsec getHitTime() const {
 		return time;
 	}
 
-	void setTime(T4nsec time) {
+	void setHitTime(T4nsec time) {
 		this->time = time;
 	}
 
+	int getHitX() const{
+		return this->m_channel.iX;
+	}
+
+	int getHitY() const{
+		return this->m_channel.iY;
+	}
 	TranslationTable::FTCAL_Index_t m_channel;
 
 protected:
@@ -80,4 +89,3 @@ private:
 };
 
 #endif // _FTCalHit_h_
-
