b7cb8ee58cee5d5dc35e7f5e13205ab0d84db26e
vallarinosimone1992 <svallarino@apcx15.ge.infn.it>
2020-07-09 13:43:34 +0200

    Correction update

--- src/libraries/FT/FTCalCluster_factory_EneCorr.h
+++ src/libraries/FT/FTCalCluster_factory_EneCorr.h
@@ -20,8 +20,13 @@ public:
 	virtual void Init();
 	virtual void ChangeRun(const std::shared_ptr<const JEvent> &aEvent);
 	virtual void Process(const std::shared_ptr<const JEvent> &aEvent);
-
 	static bool compareHits(const FTCalHit* a, const FTCalHit* b);
+
+private:
+	//I am hard-coding the indexes here, since it will always be 1 sector, 1 layer, 332 components.
+	std::map<int, double> c0, c1, c2, c3, c4; //not really true, since the components are 332, but they are not continuous from 1 to 332!
+	void CorrectClusterEnergy(FTCalCluster *cluster);
+
 };
 
 #endif /* SRC_LIBRARIES_FT_FTCALCluster_FACTORY_H_ */
--- src/libraries/FT/FTCalCluster_factory_TimeCorr.cc
+++ src/libraries/FT/FTCalCluster_factory_TimeCorr.cc
@@ -13,6 +13,10 @@
 #include <iostream>
 #include <functional>
 
+#include "JANA/Services/JParameterManager.h"
+#include "JANA/Calibrations/JCalibration.h"
+#include "JANA/Calibrations/JCalibrationManager.h"
+
 //Finestra temporale cluster
 double time_min_TimeCorr = -6;
 double time_max_TimeCorr = 6;
@@ -31,11 +35,54 @@ FTCalCluster_factory_TimeCorr::~FTCalCluster_factory_TimeCorr() {
 	// TODO Auto-generated destructor stub
 }
 
+void FTCalCluster_factory_TimeCorr::CorrectClusterEnergy(FTCalCluster *clus) {
+	auto *seed = clus->getHit(0);
+	auto component = seed->m_channel.component;
+
+	auto cc0 = this->c0[component];
+	auto cc1 = this->c1[component];
+	auto cc2 = this->c2[component];
+	auto cc3 = this->c3[component];
+	auto cc4 = this->c4[component];
+
+	auto rawEne = clus->getClusterEnergy(); //MeV
+	rawEne /= 1000.;
+
+	auto corr = (cc0 + cc1 * rawEne + cc2 * rawEne * rawEne + cc3 * rawEne * rawEne * rawEne + cc4 * rawEne * rawEne * rawEne * rawEne) / 1000.;
+	rawEne += corr;
+
+	clus->setClusterFullEnergy(rawEne * 1000); //MeV again
+
+	return;
+
+}
+
 void FTCalCluster_factory_TimeCorr::Init() {
 
 }
 void FTCalCluster_factory_TimeCorr::ChangeRun(const std::shared_ptr<const JEvent> &aEvent) {
+	static int hasLoaded = 0;
+	if (hasLoaded == 0) {
+		auto jcalib_manager = japp->GetService<JCalibrationManager>();
+		static int runN = 11324;
+		auto jcalib = jcalib_manager->GetJCalibration(11324);
 
+		//Load FT-HODO
+		std::vector<std::vector<double>> data;
+		jcalib->Get("/calibration/ft/ftcal/energycorr", data);
+
+		for (auto row : data) {
+			//sector - layer - component - c0 - c1 - c2 - c3 - c4
+			auto sector = int(row[0]);
+			auto layer = int(row[1]);
+			auto component = int(row[2]);
+			c0[component] = row[3];
+			c1[component] = row[4];
+			c2[component] = row[5];
+			c3[component] = row[6];
+			c4[component] = row[7];
+		}
+	}
 }
 void FTCalCluster_factory_TimeCorr::Process(const std::shared_ptr<const JEvent> &aEvent) {
 
@@ -69,6 +116,7 @@ void FTCalCluster_factory_TimeCorr::Process(const std::shared_ptr<const JEvent> 
 		//we do once the calculation of ALL quantities of interest here,
 		//then the "get" methods just return the computed values.
 		clusters[i]->computeCluster();
+		this->CorrectClusterEnergy(clusters[i]);
 		//std::cout <<"Is good cluster? " <<clusters[i]->isGoodCluster() <<" cluster size is " <<clusters[i]->getClusterSize() <<std::endl;
 		if (clusters[i]->isGoodCluster() == true) {
 			mData.push_back(clusters[i]);
