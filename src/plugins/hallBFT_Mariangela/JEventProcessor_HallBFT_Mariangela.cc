3c3dc967b8a6851ebbd4f921800896052d310a97
mariangela-bondi <mariangela.bondi@ct.infn.it>
2020-11-19 11:20:52 +0100

    bo

--- src/plugins/hallBFT_Mariangela/JEventProcessor_HallBFT_Mariangela.cc
+++ src/plugins/hallBFT_Mariangela/JEventProcessor_HallBFT_Mariangela.cc
@@ -438,6 +438,7 @@ void JEventProcessor_HallBFT_Mariangela::Process(const std::shared_ptr<const JEv
 
 
    auto seed_hodo = *(hits_hodo.begin());
+   cout<< seed_hodo->getHitEnergy()<<endl;
 
 	for (auto hit_hodo : hits_hodo) {
 		hHitsHODOPosition->Fill(hit_hodo->getHitDx(),hit_hodo->getHitDy());
@@ -463,7 +464,7 @@ void JEventProcessor_HallBFT_Mariangela::Process(const std::shared_ptr<const JEv
 			if(hit_hodo->m_channel.sector == 8)	EneHitHODO_layer2_sector8[hit_hodo->m_channel.component - 1]->Fill(hit_hodo->getHitEnergy());
 		}
 
-		if(seed_hodo->getHitEnergy()>=0.5) hHitsHODOTime->Fill(hit_hodo->getHitTime() - seed_hodo->getHitTime());
+//		if(seed_hodo->getHitEnergy()>=0.5) hHitsHODOTime->Fill(hit_hodo->getHitTime() - seed_hodo->getHitTime());
 	}
 
 
