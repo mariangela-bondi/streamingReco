04ff5d2299b706a5c0f8e7afa722c1ed0d460edd
mariangela-bondi <mariangela.bondi@ct.infn.it>
2020-11-19 10:59:14 +0100

    as before

--- src/plugins/hallBFT_Mariangela/JEventProcessor_HallBFT_Mariangela.cc
+++ src/plugins/hallBFT_Mariangela/JEventProcessor_HallBFT_Mariangela.cc
@@ -433,11 +433,11 @@ void JEventProcessor_HallBFT_Mariangela::Process(const std::shared_ptr<const JEv
 	 } //-------photon2
 	 } //------photon1*/
 
-	cout << "sono qua"<<endl;
+
 	std::sort(hits_hodo.begin(), hits_hodo.end(), compareHits);
 
 	 auto seed_hodo = hits_hodo.at(0);
-
+	 cout << "sono qua"<<endl;
 	for (auto hit_hodo : hits_hodo) {
 
 		hHitsHODOPosition->Fill(hit_hodo->getHitDx(),hit_hodo->getHitDy());
