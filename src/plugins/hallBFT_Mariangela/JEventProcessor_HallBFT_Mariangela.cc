5ea14c8b6ff4c9a8a312a9198a643f75cdde8c17
mariangela-bondi <mariangela.bondi@ct.infn.it>
2020-12-14 17:24:55 +0100

    debugging clustering FT cal

--- src/plugins/hallBFT_Mariangela/JEventProcessor_HallBFT_Mariangela.cc
+++ src/plugins/hallBFT_Mariangela/JEventProcessor_HallBFT_Mariangela.cc
@@ -885,7 +885,7 @@ void JEventProcessor_HallBFT_Mariangela::Process(const std::shared_ptr<const JEv
 
 		if(cluster0->getClusterFullEnergy()>500 && cluster1->getClusterFullEnergy()>500&&cluster0->getTheta()<4.5 && cluster1->getTheta()<4.5 && cluster0->getTheta()>2.5 && cluster1->getTheta()>2.5){
 		if(distance_seed<=40) pp++;
-		if(distance_seed>40 && distance_seed<=60 ) qq++;
+		if(distance_seed>40 ) qq++;
 
 			for(int i=0; i<cluster0->getClusterSize(); i++ ){
 				auto hit = cluster0->getHit(i);
@@ -893,7 +893,7 @@ void JEventProcessor_HallBFT_Mariangela::Process(const std::shared_ptr<const JEv
 					XYDCPosClusColz_dis40[pp]->Fill(hit->getHitX(), hit->getHitY(),hit->getHitEnergy());
 					XYDCPosClus0_dis40[pp]->Fill(hit->getHitX(), hit->getHitY());
 				}
-				if(distance_seed>40 && distance_seed<=60 && qq<100){
+				if(distance_seed>40 && qq<100){
 					XYDCPosClusColz_dis40_60[qq]->Fill(hit->getHitX(), hit->getHitY(),hit->getHitEnergy());
 					XYDCPosClus0_dis40_60[qq]->Fill(hit->getHitX(), hit->getHitY());
 				}
@@ -906,7 +906,7 @@ void JEventProcessor_HallBFT_Mariangela::Process(const std::shared_ptr<const JEv
 					XYDCPosClusColz_dis40[pp]->Fill(hit->getHitX(), hit->getHitY(),hit->getHitEnergy());
 					XYDCPosClus1_dis40[pp]->Fill(hit->getHitX(), hit->getHitY());
 				}
-				if(distance_seed>40 && distance_seed<=60 && qq<100){
+				if(distance_seed>40&& qq<100){
 					XYDCPosClusColz_dis40_60[qq]->Fill(hit->getHitX(), hit->getHitY(),hit->getHitEnergy());
 					XYDCPosClus1_dis40_60[qq]->Fill(hit->getHitX(), hit->getHitY());
 				}
