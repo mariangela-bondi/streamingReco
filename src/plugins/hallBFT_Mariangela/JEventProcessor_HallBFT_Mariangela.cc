f98dd8d2b6779c9167e5678ea0c89d15efae70b9
mariangela-bondi <mariangela.bondi@ct.infn.it>
2021-01-27 17:28:59 +0100

    cut at 3 degree

--- src/plugins/hallBFT_Mariangela/JEventProcessor_HallBFT_Mariangela.cc
+++ src/plugins/hallBFT_Mariangela/JEventProcessor_HallBFT_Mariangela.cc
@@ -667,7 +667,7 @@ void JEventProcessor_HallBFT_Mariangela::Process(const std::shared_ptr<const JEv
    //  cout << clusters.size()<< " "<< gammas.size()<<" "<< Ngamma<<endl;
 	hNclustervsNgamma->Fill(clusters.size(), Ngamma);
 
-	if(gammas.size()==2){
+	if(gammas.size()>2){
 
 		for(int j=0; j<gammas.size(); j++){
           auto gamma1 = gammas[j];
@@ -690,11 +690,11 @@ void JEventProcessor_HallBFT_Mariangela::Process(const std::shared_ptr<const JEv
 
   		//	cout << z01<< " "<<M01<<endl;
   			hpi0->Fill(M01);
-  	  		if(gamma1->getParticleEnergy()>500 && gamma2->getParticleEnergy()>500 && acos(z01)>0.035){
+  	  		if(gamma1->getParticleEnergy()>500 && gamma2->getParticleEnergy()>500 && acos(z01)*(180. / M_PI)>3.){
   	  			hpi0_angle->Fill(M01);
   	  		}
   if(theta_gamma1<4.5 && theta_gamma2<4.5 && theta_gamma1>2.5 && theta_gamma2>2.5) {
-  		if(gamma1->getParticleEnergy()>500 && gamma2->getParticleEnergy()>500 && acos(z01)*(180. / M_PI)>2.){
+  		if(gamma1->getParticleEnergy()>500 && gamma2->getParticleEnergy()>500 && acos(z01)*(180. / M_PI)>3.){
   			hpi0_raffa->Fill(M01);
   		}
   }
@@ -804,10 +804,10 @@ void JEventProcessor_HallBFT_Mariangela::Process(const std::shared_ptr<const JEv
 			hInVMass_angle->Fill(M, acos(z)*180/3.1415);
 			hInVMass_distance->Fill(M, distance_seed);
 			hangle_distance->Fill(acos(z)*180/3.1415, distance_seed);
-			if(acos(z)>0.035) hDCInvariantMass_angle->Fill(M);
+			if(acos(z01)*(180. / M_PI)>3.) hDCInvariantMass_angle->Fill(M);
 
 	if(cluster0->getTheta()<4.5 && cluster1->getTheta()<4.5 && cluster0->getTheta()>2.5 && cluster1->getTheta()>2.5) {
-		if(cluster0->getClusterFullEnergy()>500 && cluster1->getClusterFullEnergy()>500 && acos(z)>0.04){   //0.035 rad = 2 deg
+		if(cluster0->getClusterFullEnergy()>500 && cluster1->getClusterFullEnergy()>500 && acos(z01)*(180. / M_PI)>3.){   //0.035 rad = 2 deg
 		hDCInvariantMass_Raffa->Fill(M);
 		}
 		}
